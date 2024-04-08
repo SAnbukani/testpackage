bc5db3de141929d1357a0000092640165623e263
Anbukani <anbu.s@talentakeaways.com>
2024-04-08 14:33:25 +0530

    First commit

---
+++ src/SampleTest.php
@@ -0,0 +1,11 @@
+<?php
+	namespace TTIPL\Infrastructure;
+	
+	class SampleTest
+	{
+		public function test_package(String $inputText)
+		{
+			return 'Welcome to our new package '.$inputText.'. Good to continue';
+		}
+	}
+?>
